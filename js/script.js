d9f35ef39faad76c768817338c72a5ec64ceee32
Débora <debora.rubio.soliva@gmail.com>
2023-01-12 19:11:09 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+function calculator(name1, name2){
+    var n = (Math.random() * 100).toFixed(2);
+    var msg;
+
+    if (n > 80) {
+        msg = 'Sois almas gemelas!';
+    } else if (n > 80){
+        msg = 'Hay posibilidades';
+    } else {
+        msg = 'No sois nada compatibles, buscate a otr@!';
+    }
+
+    return {
+        value: n,
+        message: msg,
+        name1: name1,
+        name2: name2
+    };
+    //return [n, msg, name1, name2]
+}
+
+function calcular(){
+    document.getElementById('loading').classList.remove('hidden');
+    document.getElementById('messages').classList.remove('hidden');
+
+    var name1 = document.getElementById('name1').value;
+    var name2 = document.getElementById('name2').value;
+
+    var afinidad = calculator(name1, name2);
+
+    setTimeout(function(){
+        document.getElementById('loading').classList.add('hidden');
+        
+        var comment = document.getElementById('result');
+        comment.innerHTML = afinidad.message;
+        comment.classList.remove('hidden');
+    
+        var value = document.getElementById('value');
+        value.classList.remove('hidden');
+        value.innerHTML = afinidad.name1 + ' y ' + afinidad.name2 + ', vuestra afinidad es:' + afinidad.value + '%';
+    }, 3000)
+
+    // Solicitud GET (Request).
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        // Exito
+        .then(response => response.json()) // convertir a json
+        .then(json => console.log(json)) //imprimir los datos en la consola
+        .catch(err => console.log('Solicitud fallida', err)) // Capturar errores
+
+}
